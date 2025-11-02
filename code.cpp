d23f2a14ffe6ea254c47cab6c95eec3894628e22
fahadjan2 <fahadfawad2006@gmail.com>
2025-11-01 21:43:28 -0700

    Commit

--- code.cpp
+++ code.cpp
@@ -17,7 +17,8 @@ void output(vector<vector<vector<milliseconds>>> &durations);
 
 int main() {
     string filename = "codes.txt";
-    vector<vector<vector<milliseconds>>> durations; //3D array
+    vector<vector<vector<milliseconds>>> durations; //3D array, holds [#run][# indicating either vect, set, list][# indicating read/sort/etc duration]
+    //                                                                       0 = vect, 1 = set, 2 = list           0 = read, 1 = sort, 2 = insert, 3 = delete         
     
     vectorracer(filename, durations);
     listracer(filename, durations);
@@ -29,83 +30,84 @@ int main() {
 }
 
 void vectorracer(const string filename, vector<vector<vector<milliseconds>>> &durations) {
-    vector<string> vect;
-    //Read
-    ifstream file(filename);
-    auto start = high_resolution_clock::now();
-    string value;
-    while (file >> value) {
-        vect.push_back(value);
-    };
-    auto end = high_resolution_clock::now();
-    auto readduration = duration_cast<milliseconds>(end - start);
+    for (int i = 0; i < 15; i++) {
+        vector<string> vect;
+        //Read
+        ifstream file(filename);
+        auto start = high_resolution_clock::now();
+        string value;
+        while (file >> value) {
+            vect.push_back(value);
+        };
+        auto end = high_resolution_clock::now();
+        auto readduration = duration_cast<milliseconds>(end - start);
 
-    //Sort
-    start = high_resolution_clock::now();
-    sort(vect.begin(), vect.end());
-    end = high_resolution_clock::now();
-    auto sortduration = duration_cast<milliseconds>(end - start);
-        
-    //Insert
-    value = "TESTCODE";
-    start = high_resolution_clock::now();
-    auto it = vect.begin();
-    advance(it, vect.size() / 2);
-    vect.insert(it, value);
-    end = high_resolution_clock::now();
-    auto insertduration = duration_cast<milliseconds>(end - start);
+        //Sort
+        start = high_resolution_clock::now();
+        sort(vect.begin(), vect.end());
+        end = high_resolution_clock::now();
+        auto sortduration = duration_cast<milliseconds>(end - start);
+            
+        //Insert
+        value = "TESTCODE";
+        start = high_resolution_clock::now();
+        auto it = vect.begin();
+        advance(it, vect.size() / 2);
+        vect.insert(it, value);
+        end = high_resolution_clock::now();
+        auto insertduration = duration_cast<milliseconds>(end - start);
 
-    //Delete
-    start = high_resolution_clock::now();
-    it = vect.begin();
-    vect.erase(it);
-    end = high_resolution_clock::now();
-    auto deleteduration = duration_cast<milliseconds>(end - start);
+        //Delete
+        start = high_resolution_clock::now();
+        it = vect.begin();
+        vect.erase(it);
+        end = high_resolution_clock::now();
+        auto deleteduration = duration_cast<milliseconds>(end - start);
 
-    //vector duration set
-    vector<milliseconds> dur;
-    dur.push_back(readduration);
-    dur.push_back(sortduration);
-    dur.push_back(insertduration);
-    dur.push_back(deleteduration);
-
-    return dur;
+        //duration set
+        durations[i][0][0] = readduration;
+        durations[i][0][1] = sortduration;
+        durations[i][0][2] = insertduration;
+        durations[i][0][3] = deleteduration;
+    }
+    return;
 }
 
 void setracer(const string filename, vector<vector<vector<milliseconds>>> &durations) {
-    set<string> set;
-    //Read
-     ifstream file(filename);
-    auto start = high_resolution_clock::now();
-    string value;
-    while (file >> value) {
-        set.insert(value);
-    };
-    auto end = high_resolution_clock::now();
-    auto readduration = duration_cast<milliseconds>(end - start);
+    for (int i = 0; i < 15; i++) {
+        set<string> set;
+        //Read
+        ifstream file(filename);
+        auto start = high_resolution_clock::now();
+        string value;
+        while (file >> value) {
+            set.insert(value);
+        };
+        auto end = high_resolution_clock::now();
+        auto readduration = duration_cast<milliseconds>(end - start);
 
-    //Insert
-    value = "TESTCODE";
-    start = high_resolution_clock::now();
-    auto it = set.begin();
-    advance(it, set.size() / 2);
-    set.insert(it, value);
-    end = high_resolution_clock::now();
-    auto insertduration = duration_cast<milliseconds>(end - start);
+        //Insert
+        value = "TESTCODE";
+        start = high_resolution_clock::now();
+        auto it = set.begin();
+        advance(it, set.size() / 2);
+        set.insert(it, value);
+        end = high_resolution_clock::now();
+        auto insertduration = duration_cast<milliseconds>(end - start);
 
-    //Delete
-    start = high_resolution_clock::now();
-    it = set.begin();
-    set.erase(it);
-    end = high_resolution_clock::now();
-    auto deleteduration = duration_cast<milliseconds>(end - start);
+        //Delete
+        start = high_resolution_clock::now();
+        it = set.begin();
+        set.erase(it);
+        end = high_resolution_clock::now();
+        auto deleteduration = duration_cast<milliseconds>(end - start);
 
-   //duration set;
-    durations.push_back;
-    dur.push_back(insertduration);
-    dur.push_back(deleteduration);
-
-    return dur;
+        //duration set
+        durations[i][1][0] = readduration;
+        durations[i][1][2] = insertduration;
+        durations[i][1][3] = deleteduration;
+    }
+    return;
 }
 
 void listracer(const string filename, vector<vector<vector<milliseconds>>> &durations) {
@@ -143,14 +145,13 @@ void listracer(const string filename, vector<vector<vector<milliseconds>>> &dura
         end = high_resolution_clock::now();
         auto deleteduration = duration_cast<milliseconds>(end - start);
 
-        //Struct duration set
-        Durations dur;
-        dur.read = readduration;
-        dur.sort = sortduration;
-        dur.insert = insertduration;
-        dur.delet = deleteduration;
+        //duration set
+        durations[i][2][0] = readduration;
+        durations[i][2][1] = sortduration;
+        durations[i][2][2] = insertduration;
+        durations[i][2][3] = deleteduration;
     }
-    return dur;
+    return;
 }
 
 //Outputs the three different durations
